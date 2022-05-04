9e0f52dd328261584528fffe854a996be180a7dd
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-05-05 00:57:46 +0700

    learn to create dynamic data type

---
+++ bin/DynamicDataType.dart
@@ -0,0 +1,12 @@
+void main() {
+
+  // Keyword dynamic can to use to added much data with such diffrent type
+  dynamic variabel = 100;
+  print(variabel);
+  
+  variabel = true;
+  print(variabel);
+  
+  variabel = 'Frans';
+  print(variabel);
+}
