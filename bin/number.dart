b74c3fcb70a41655285917dea2ddc981ed10bdca
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-05-05 00:38:09 +0700

    learn create data integer in dart

---
+++ bin/number.dart
@@ -0,0 +1,15 @@
+void main() {
+  // at dart we cant create number use int ussualy not use decimal number and double for decimal number data
+  int number1 = 10;
+  double number2 = 10.5;
+
+  // Tipe data use num this can use to change data int or double
+  num number3 = 10;
+  num number4 = 10.5;
+
+  print(number1);
+  print(number2);
+  print('\n This use num');
+  print(number3);
+  print(number4);
+}
