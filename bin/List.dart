289ea9527a7750b1e88f35281056927aa7e6113a
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-05-05 23:45:27 +0700

    learn list dart

--- bin/List.dart
+++ bin/List.dart
@@ -8,9 +8,33 @@ void main() {
   print(listInt);
   print(listString);
 
-  // Added data to array
+  // Added data to array this manipulation array
   print('Example 2');
   var names = <String>[];
 
+  names.add('Stefanus');
+  names.add('Frans');
+  names.add('Sebastian');
 
+  print(names);
+  print(names.length);
+
+  // Learn manipulation data array
+
+  print(names[1]);
+
+  names[1] = 'Joko';
+  print(names[1]);
+
+  names.removeAt(2);
+  print(names);
+
+  // Direct declaration list
+  var nameSchools = <String>[
+    'SMAKFS',
+    'SMA 01',
+    'SMA 02',
+  ];
+  print('\nExample');
+  print(nameSchools);
 }
