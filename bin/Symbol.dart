0344e5cf597cf9632e21f43119ea2f883cfe47d4
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-05-06 01:56:26 +0700

    learn create symbol at dart

---
+++ bin/Symbol.dart
@@ -0,0 +1,10 @@
+void main() {
+
+  Symbol symbol1 = Symbol("Frans Sebastian");
+  // At dart we cant to added spasing we have to use underscore
+  var symbol2 = #lern_dart;
+
+  print(symbol1);
+  print(symbol2);
+
+}
