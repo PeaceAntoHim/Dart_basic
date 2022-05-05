5f255cac1972da9fe2f1db7669c83f41f2420b68
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-05-05 23:31:09 +0700

    learn operator type test

---
+++ bin/List.dart
@@ -0,0 +1,16 @@
+void main() {
+
+  print('Example 1');
+  List<int> listInt = [];
+
+  var listString = <String>[];
+
+  print(listInt);
+  print(listString);
+
+  // Added data to array
+  print('Example 2');
+  var names = <String>[];
+
+
+}
--- bin/OperatorTypeTest.dart
+++ bin/OperatorTypeTest.dart
@@ -1,5 +1,20 @@
 void main() {
 
+  dynamic variable = 100;
 
+  // This will declare var to integer
+  var variableInt = variable as int;
+
+  var isInt = variable is int;
+  var isNotBoolean = variable is! bool;
+
+  print(variableInt);
+  print(isInt);
+  print(isNotBoolean);
+  print(variable is String);
+
+  print(variable is! int);
+  print(variable is! bool);
+  print(variable is! String);
 
 }
