a6271b1a15157412fcf07c2a9cd8e1ae9ef5289c
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-05-06 16:21:49 +0700

    learn do while loop

---
+++ bin/DoWhileLoop.dart
@@ -0,0 +1,10 @@
+void main() {
+
+  var counter = 1;
+
+  do {
+    print('Loop to-$counter');
+    counter++;
+  } while (counter <= 10);
+
+}
---
+++ bin/WhileLoop.dart
@@ -0,0 +1,9 @@
+void main() {
+
+  var counter = 1;
+
+  while(counter <= 10) {
+    print('Loop to-$counter');
+    counter++;
+  }
+}
