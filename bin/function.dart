6a5fba8ee10ece7f8dea7cc4cf2e5311e361ab20
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-05-06 16:56:54 +0700

    learn how to create function at dart

--- bin/function.dart
+++ bin/function.dart
@@ -1,8 +1,29 @@
+// Function without parameter/argument
 void sayHello() {
   print('Hello world');
 }
 
+// Function use parameter/argument
+void sayHallo(String firstName, String lastName) {
+  print('Hello $firstName $lastName');
+}
+
+// Function use optional parameter have to nullable added square bracket can change the data will not must fillable
+// Use optional parameter can't be use at front we cant use at back
+void sayHay(String firstName, [String? middleName, String? lastName]) {
+  print('Hello $firstName $middleName $lastName');
+}
+
+// We can to added default value at optional parameter
+void sayHi(String firstName, [String middleName = '', String lastName = '']) {
+  print('Hello $firstName $middleName $lastName');
+}
+
 void main() {
   sayHello();
-  sayHello();
+  sayHallo('Frans', 'Sebastian');
+  sayHay('Frans');
+  sayHay('Stefanus', 'Frans');
+  sayHay('Stefanus', 'Frans', 'Sebastian');
+  sayHi('Frans');
 }
