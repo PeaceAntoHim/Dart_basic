048b7ed89c7d0a14a5997c403e84431dcb77a8f9
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-05-06 15:04:40 +0700

    learn if else statment

---
+++ bin/ifelse.dart
@@ -0,0 +1,18 @@
+void main() {
+
+  var nilai = 80;
+  var absen = 70;
+
+  if (nilai >= 75 && absen >= 75) {
+    print('Your Grade A');
+  } else if (nilai >= 70 && absen >= 70) {
+    print('Your Grade B');
+  } else if (nilai >= 60 && absen >= 60) {
+    print('Your Grade C');
+  } else if (nilai >=  50 && absen >= 50) {
+    print('Nilai Grade D');
+  } else {
+    print('Your Garde E');
+  }
+
+}
