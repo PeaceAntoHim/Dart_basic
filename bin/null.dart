61fefb21d3fcf005ed0e53bb114a4e7f30712642
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-05-06 15:16:53 +0700

    learn to create nullable data type

---
+++ bin/null.dart
@@ -0,0 +1,12 @@
+void main() {
+
+  // Learn how to create data type nullable
+  int number1;
+  number1 = 10;
+
+  int? number2; // This are how to create nullable data type
+
+  print(number1);
+  print(number2);
+
+}
