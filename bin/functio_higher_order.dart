524135f808c02ba28f7759722d69d5161b683903
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-05-07 00:29:29 +0700

    learn to create function higher order

---
+++ bin/functio_higher_order.dart
@@ -0,0 +1,22 @@
+// Made function high order function to filter data
+void sayHello(String name, String Function(String) filter) {
+  var filteredName = filter(name);
+  print('Hi $filteredName');
+}
+
+// This function to filter word use
+String filterBadWord(String name) {
+  if (name == 'gila' || name == 'Gila' || name == 'GILA') {
+    return "****";
+  } else {
+    return name;
+  }
+}
+
+
+void main() {
+  sayHello('Frans', filterBadWord);
+  sayHello('gila', filterBadWord);
+  sayHello('Gila', filterBadWord);
+  sayHello('GILA', filterBadWord);
+}
